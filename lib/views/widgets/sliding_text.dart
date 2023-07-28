ee9612dd260b5b536a2e63b1ededc0f843205743
MeDoRa0 <mohamed.hossam93x@gmail.com>
2023-07-28 19:51:10 +0300

    add sliding animation to splash view

---
+++ lib/views/widgets/sliding_text.dart
@@ -0,0 +1,27 @@
+//this widget make text sliding
+import 'package:flutter/material.dart';
+
+class SlidingText extends StatelessWidget {
+  const SlidingText({
+    super.key,
+    required this.slidingAnimation,
+  });
+
+  final Animation<Offset> slidingAnimation;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+        animation: slidingAnimation,
+        builder: (context, _) {
+          return SlideTransition(
+            position: slidingAnimation,
+            child: Text(
+              'Read Free Books',
+              //this make text in center
+              textAlign: TextAlign.center,
+            ),
+          );
+        });
+  }
+}
--- lib/views/widgets/splash_view_body.dart
+++ lib/views/widgets/splash_view_body.dart
@@ -1,8 +1,41 @@
 import 'package:bookly/images/app_images.dart';
 import 'package:flutter/material.dart';
+import 'sliding_text.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+//SingleTickerProviderStateMixin is to handle the change rate of valus
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  //this to make animation on screen
+  //late tell that code will get its value in future
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+  @override
+  void initState() {
+    super.initState();
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    );
+    slidingAnimation =
+        Tween<Offset>(begin: const Offset(0, 4), end: Offset.zero)
+            .animate(animationController);
+    animationController.forward();
+  }
+
+  //we must make dispose with every controlller to prevent memory leak
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    animationController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,11 +44,8 @@ class SplashViewBody extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         Image.asset(Assets.imagesLogo),
-        Text(
-          'Read Free Books',
-          //this make text in center
-          textAlign: TextAlign.center,
-        ),
+        //AnimatedBuilder will make this widget rebuild when animation value change
+        SlidingText(slidingAnimation: slidingAnimation),
       ],
     );
   }
