3ea22c0a32ba19078bb95f57faed5054db80c098
MeDoRa0 <mohamed.hossam93x@gmail.com>
2023-07-29 03:40:57 +0300

    add featured list view to home

--- lib/Features/home/presentation/views/widgets/custom_list_view_item.dart
+++ lib/Features/home/presentation/views/widgets/custom_list_view_item.dart
@@ -2,29 +2,26 @@
 import 'package:bookly/images/app_images.dart';
 import 'package:flutter/material.dart';
 
-class CustomListviewItem extends StatelessWidget {
-  const CustomListviewItem({super.key});
+class FeaturedListViewItem extends StatelessWidget {
+  const FeaturedListViewItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * .3,
-      child: AspectRatio(
-        //asspectRatio will change the width and heghit width/height
-        aspectRatio: 2.7 / 4,
-        child: Container(
-          //I disable the code below because i use AspectRatio widget instead
-          //MediaQuery will adjust the height and width of container in term of screen dimensions
-          //height: MediaQuery.of(context).size.height * .25,
-          //width: 50,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(16),
-            color: Colors.white,
-            image: const DecorationImage(
-              fit: BoxFit.fill,
-              image: AssetImage(
-                Assets.imagesTestImage,
-              ),
+    return AspectRatio(
+      //asspectRatio will change the width and heghit width/height
+      aspectRatio: 2.7 / 4,
+      child: Container(
+        //I disable the code below because i use AspectRatio widget instead
+        //MediaQuery will adjust the height and width of container in term of screen dimensions
+        //height: MediaQuery.of(context).size.height * .25,
+        //width: 50,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          color: Colors.white,
+          image: const DecorationImage(
+            fit: BoxFit.fill,
+            image: AssetImage(
+              Assets.imagesTestImage,
             ),
           ),
         ),
--- lib/Features/home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'custom_app_bar.dart';
 import 'custom_list_view_item.dart';
+
 //this is home screen body
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
@@ -10,10 +11,29 @@ class HomeViewBody extends StatelessWidget {
     return const Column(
       children: [
         CustomAppBar(),
-        CustomListviewItem(),
+        FeaturedBooksListview(),
       ],
     );
   }
 }
 
+class FeaturedBooksListview extends StatelessWidget {
+  const FeaturedBooksListview({super.key});
 
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * .3,
+      child: ListView.builder(
+        //scrolldirection will make the list scroll horizentally
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) {
+          return const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 8),
+            child: FeaturedListViewItem(),
+          );
+        },
+      ),
+    );
+  }
+}
