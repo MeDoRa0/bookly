ccebc752448f69eca51266fbf954bae8ebadb3a9
MeDoRa0 <mohamed.hossam93x@gmail.com>
2023-07-31 05:02:36 +0300

    best seller item part 2

--- lib/Features/home/presentation/views/widgets/custom_app_bar.dart
+++ lib/Features/home/presentation/views/widgets/custom_app_bar.dart
@@ -8,7 +8,7 @@ class CustomAppBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(left: 24, right: 24, top: 50, bottom: 10),
+      padding: const EdgeInsets.only(left: 30, top: 60, bottom: 47),
       child: Row(
         //logo in app bar
         children: [
--- lib/Features/home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -15,18 +15,21 @@ class HomeViewBody extends StatelessWidget {
       children: [
         CustomAppBar(),
         Padding(
-          padding: EdgeInsets.only(left: 16),
+          padding: EdgeInsets.only(left: 30),
           child: FeaturedBooksListview(),
         ),
         SizedBox(
           height: 50,
         ),
         Padding(
-          padding: EdgeInsets.symmetric(horizontal: 25),
+          padding: EdgeInsets.symmetric(horizontal: 30),
           child: Text(
             'Best Seller',
             style: Styles.textStyle18,
           ),
+        ),
+        SizedBox(
+          height: 20,
         ),
         BestSellerItem(),
       ],
@@ -41,28 +44,46 @@ class BestSellerItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return SizedBox(
       //change the height to 105 after finish best seller list
-      height: 100,
-      width: 70,
+      height: 125,
+      //width: 70,
       child: Row(
         children: [
-          AspectRatio(
-            //aspectRatio will change the width and heghit width/height
-            aspectRatio: 2.5 / 4,
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(8),
-                color: Colors.white,
-                image: const DecorationImage(
-                  fit: BoxFit.fill,
-                  image: AssetImage(
-                    Assets.imagesTestImage,
+          Padding(
+            padding: const EdgeInsets.only(left: 30),
+            child: AspectRatio(
+              //aspectRatio will change the width and heghit width/height
+              aspectRatio: 2.6 / 4,
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8),
+                  color: Colors.white,
+                  image: const DecorationImage(
+                    fit: BoxFit.fill,
+                    image: AssetImage(
+                      Assets.imagesTestImage,
+                    ),
                   ),
                 ),
               ),
             ),
           ),
+          const SizedBox(
+            width: 30,
+          ),
           Column(
-            children: [],
+            children: [
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.5,
+                child: const Text(
+                  'Harry Potter and the Goblet of Fire',
+                  style: Styles.textStyle20,
+                  //maxLines will limit how many lines the text can take
+                  maxLines: 2,
+                  //this will put ... at end of text if it overflow
+                  overflow: TextOverflow.ellipsis,
+                ),
+              )
+            ],
           )
         ],
       ),
