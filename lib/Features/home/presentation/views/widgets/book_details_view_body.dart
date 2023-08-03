fd1840f049ed591924eeabb22daa06dd4bccf282
MeDoRa0 <mohamed.hossam93x@gmail.com>
2023-08-03 22:27:27 +0300

    creat book details view

---
+++ lib/Features/home/presentation/views/widgets/book_details_view_body.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Book_details_view_body extends StatelessWidget {
+  const Book_details_view_body({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
