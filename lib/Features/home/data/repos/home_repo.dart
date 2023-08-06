dbe1b5395ec0bbad2ac9ed9271db213871561bab
MeDoRa0 <mohamed.hossam93x@gmail.com>
2023-08-06 23:29:07 +0300

    creat home repo method and failure file

--- lib/Features/home/data/repos/home_repo.dart
+++ lib/Features/home/data/repos/home_repo.dart
@@ -1,4 +1,10 @@
+import 'package:bookly/Features/home/data/models/book_model/book_model.dart';
+import 'package:bookly/core/errors/failures.dart';
+import 'package:dartz/dartz.dart';
+
 abstract class HomeRepo {
-  fetchBestSellerBooks();
-  fetchFeaturedBooks(); 
+  //future because it will take time.
+  //i can determine 2 types using Either, it take 2 sides, the right side will return list of book model and the left side will return Failure
+  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks();
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks();
 }
---
+++ lib/core/errors/failures.dart
@@ -0,0 +1,4 @@
+// this class will contain all errors may happen in this project
+abstract class Failure {}
+
+class ServerFailure extends Failure {}
